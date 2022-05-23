3ef3c1df09c2a77741161fe5b5aa4661e2692281
Julia Clepf <jumgc09@gmail.com>
2022-05-23 12:40:08 -0300

    feat: add rows to store and product table

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -17,7 +17,7 @@ class StoreFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->words(3, true),'description' => $this->faker->sentence
         ];
     }
 }
